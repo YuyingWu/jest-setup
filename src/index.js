91b0f7266dbd23278519b293d9cc3f588ae9ddf3
jiwu.wyy <jiwu.wyy@alibaba-inc.com>
2018-04-14 17:49:05 +0800

    build: react dependencies

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-import './App';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+const mountNode = document.getElementById('app');
+ReactDOM.render(<App />, mountNode);
